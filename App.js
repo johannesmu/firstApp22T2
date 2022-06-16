133877eed6113a0e69340df8e04393788f378be8
johannes <jmuljana@gmail.com>
2022-06-16 15:14:31 +1000

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MyComponent } from './components/MyComponent'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello there! It's our first app!</Text>
       <StatusBar style="auto" />
+      <MyComponent text="Make me a sandwich!" color="red" size={32} />
+      <MyComponent text="I like donuts!" color="green" size={18} />
     </View>
   );
 }
