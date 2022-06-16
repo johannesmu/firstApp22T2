7f45d0ac49ea39d1c26f6ac57fd4364bf653ce27
johannes <jmuljana@gmail.com>
2022-06-16 15:27:06 +1000

    adding button component

---
+++ components/MyButton.js
@@ -0,0 +1,22 @@
+import { TouchableOpacity, Text, StyleSheet  } from "react-native"
+import { useState } from "react"
+
+export function MyButton ( props ) {
+  const[ click, setClick ] = useState(0)
+
+  return(
+    <TouchableOpacity style={ styles.button }>
+      <Text style={styles.buttonText} >Received {click} clicks</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "black",
+    padding: 10,
+  },
+  buttonText: {
+    color: "white"
+  }
+})
